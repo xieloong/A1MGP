8941b654a046ca0d1ffb397ebc683f22b1e4edfc
KneeGah <56944321+xieloong@users.noreply.github.com>
2021-12-07 17:47:18 +0800

    movement up and down and changed sprite + animation

--- app/src/main/java/com/example/week4real/Smurf.java
+++ app/src/main/java/com/example/week4real/Smurf.java
@@ -12,15 +12,22 @@ import java.util.Random;
 public class Smurf implements EntityBase {
     private boolean isDone = false;
     private Bitmap bmp = null, scaledbmp = null;
+
+    boolean IsGoingUp = false;
+
     int ScreenWidth, ScreenHeight;
     private float xPos, yPos, offset;
+
+    int spriteCounter = 0;
+
     private SurfaceView view = null;
     Matrix tfx = new Matrix();
     DisplayMetrics metrics;
 
     float lifetime;
 
-    private Sprite spritesmurf = null;   // New on Week 8
+    private Sprite spritesmurf = null;
+
 
     private boolean hasTouched = false; // New to Week 8
 
@@ -37,20 +44,24 @@ public class Smurf implements EntityBase {
 
     @Override
     public void Init(SurfaceView _view) {
-        //bmp = BitmapFactory.decodeResource(_view.getResources(), R.drawable.ship2_1);
+        bmp = BitmapFactory.decodeResource(_view.getResources(), R.drawable.ship2_1);
         bmp = ResourceManager.Instance.GetBitmap( R.drawable.ship2_1);
+
+        metrics = _view.getResources().getDisplayMetrics();
+
+        ScreenHeight = metrics.heightPixels;
+        ScreenWidth = metrics.widthPixels;
 
         // New to Week 8
         // Using Sprite animation class to load our sprite sheet
-        spritesmurf = new Sprite(ResourceManager.Instance.GetBitmap(R.drawable.smurf_sprite),4,4, 16 );
+        spritesmurf = new Sprite(ResourceManager.Instance.GetBitmap(R.drawable.mainsprite),1,6, 5 );
+
 
         // Define how we want the player to react or if it is enemy or obstacles, how it is going to appear as.
         // You should have this part!
-        Random ranGen = new Random();
-        xPos = ranGen.nextFloat() * _view.getWidth();
-        yPos = ranGen.nextFloat() * _view.getHeight();
 
-        lifetime = 30.0f;
+        xPos = 0.1f * ScreenWidth;
+        yPos = 0.7f * ScreenHeight;
     }
 
     @Override
@@ -58,50 +69,67 @@ public class Smurf implements EntityBase {
         // Week 8
         spritesmurf.Update(_dt);
 
-        lifetime -= _dt;
-        if (lifetime < 0.0f) {
-            SetIsDone(true);   // <--- This part here or this code, meant when time is up, kill the items.
-        }
+//        lifetime -= _dt;
+//        if (lifetime < 0.0f) {
+//            SetIsDone(true);   // <--- This part here or this code, meant when time is up, kill the items.
+//        }
 
-        if (TouchManager.Instance.IsDown()) {   // Previous and it is for just a touch - useful for collision with a image (example button)
-
-            // Check collision
-            float imgRadius = bmp.getHeight() * 0.5f;
-            if (Collision.SphereToSphere(TouchManager.Instance.GetPosX(), TouchManager.Instance.GetPosY(), 0.0f, xPos, yPos, imgRadius)) {
-                SetIsDone(true); // // <--- This part here or this code, meant when time is up, kill the items.
-                // If it is a button --> Going to another state
-            }
+        if (TouchManager.Instance.IsDown())
+        {   // Previous and it is for just a touch - useful for collision with a image (example button)
+            IsGoingUp = true;
+            spriteCounter = 1;
         }
-        if (TouchManager.Instance.HasTouch())  // Touch and drag
+        else
         {
-            // Check collision with the smurf sprite
-            float imgRadius1 = spritesmurf.GetWidth() * 0.5f;
-            if (Collision.SphereToSphere(TouchManager.Instance.GetPosX(), TouchManager.Instance.GetPosY(), 0.0f, xPos, yPos, imgRadius1) || hasTouched) {
-                hasTouched = true;
-                xPos = TouchManager.Instance.GetPosX();
-                yPos = TouchManager.Instance.GetPosY();
-
-            }
+            IsGoingUp = false;
+            spriteCounter = 0;
         }
+
+        if (IsGoingUp)
+        {
+            yPos -= 300 * _dt;
+        }
+        else
+        {
+            yPos += 300 * _dt;
+        }
+
+        if (yPos < spritesmurf.GetHeight() / 2)
+        {
+            yPos = spritesmurf.GetHeight() / 2;
+        }
+
+        if(yPos > 0.7f * ScreenHeight)
+        {
+            yPos = 0.7f * ScreenHeight;
+        }
+
+//        if (TouchManager.Instance.HasTouch())  // Touch and drag
+//        {
+//            // Check collision with the smurf sprite
+//            float imgRadius1 = spritesmurf.GetWidth() * 0.5f;
+//            if (Collision.SphereToSphere(TouchManager.Instance.GetPosX(), TouchManager.Instance.GetPosY(), 0.0f, xPos, yPos, imgRadius1) || hasTouched) {
+//                hasTouched = true;
+//                xPos = TouchManager.Instance.GetPosX();
+//                yPos = TouchManager.Instance.GetPosY();
+//
+//            }
+//        }
     }
     @Override
     public void Render(Canvas _canvas) {
             // New to Week 8
-            spritesmurf.Render(_canvas, 100, 100);  // Location can be changed!
-
-            // New Week 8
-            if (TouchManager.Instance.HasTouch())  // Touch and drag
+//            spritesmurf.Render(_canvas, (int)xPos, (int)yPos);  // Location can be changed!
+            if (spriteCounter == 0)
             {
-                // Check collision with the smurf sprite
-                float imgRadius1 = spritesmurf.GetWidth() * 0.5f;
-                if (Collision.SphereToSphere(TouchManager.Instance.GetPosX(), TouchManager.Instance.GetPosY(), 0.0f, xPos,yPos, imgRadius1) || hasTouched)
-                {
-                    hasTouched = true;
-                    xPos = TouchManager.Instance.GetPosX();
-                    yPos = TouchManager.Instance.GetPosY();
-
-                }
+                spritesmurf.Render(_canvas, (int)xPos, (int)yPos);
             }
+            else
+            {
+                spritesmurf.Render(_canvas, (int)xPos, (int)yPos);
+            }
+
+
     }
 
     @Override
@@ -121,12 +149,12 @@ public class Smurf implements EntityBase {
 
     @Override
     public float GetPositionX() {
-        return 0;
+        return xPos;
     }
 
     @Override
     public float GetPositionY() {
-        return 0;
+        return yPos;
     }
 
     @Override
